30fe266511c5784f1f7d3318d609a40ffb8c815a
vinay <vinaypand5@gmail.com>
2019-05-27 13:52:45 +0530

    working app

--- inference/app/views.py
+++ inference/app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template,request,redirect,jsonify,make_response
+from flask import render_template,request,redirect,jsonify,make_response,url_for
 from flask import request, redirect
 from werkzeug.utils import secure_filename
 import os
@@ -8,57 +8,43 @@ import os
 def index():
 	return render_template("public/index.html")
 
-@app.route("/json_check", methods=["POST"])
-def json_check():
-    if request.is_json:
-        req = request.get_json()
-        response = {
-        	"message": "JSON received !",
-        	"name": req.get("name")
-        }
-        res=make_response(jsonify(response),200)
-        app.logger.info('200')
-    else:
-    	res=make_response(jsonify({'message':'No Json received'}),400)
-    	app.logger.info('400')
-    return res
-
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-app.config["FILE_UPLOADS"] = "/static/img/"
-app.config["ALLOWED_FILE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
-app.config["MAX_FILE_FILESIZE"] = 0.5 * 1024 * 100000000024
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static','file')
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["ALLOWED_EXTENSIONS"] = ["JSON"]
+app.config["MAX_CONTENT_LENGTH"] = 16*1024*100024
 
 def allowed_file(filename):
     if not "." in filename:
         return False
     ext = filename.rsplit(".", 1)[1]
-    if ext.upper() in app.config["allowed_file_EXTENSIONS"]:
+    if ext.upper() in app.config["ALLOWED_EXTENSIONS"]:
         return True
     else:
         return False
 
-def allowed_file_filesize(filesize):
-    if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+def allowed_filesize(filesize):
+    if int(filesize) <= app.config["MAX_CONTENT_LENGTH"]:
         return True
     else:
         return False
 
 @app.route("/upload_file", methods=["GET", "POST"])
 def upload_file():
-    if request.method == "POST":
-        if request.files:
-            if "filesize" in request.cookies:
-                if not allowed_file_filesize(request.cookies["filesize"]):
-                    app.logger.info("Filesize exceeded maximum limit")
-                    return redirect(request.url)
-                file = request.files["file"]
-                if allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    file.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
-                    app.logger.info("Image saved")
-                    return redirect(request.url)
-                else:
-                    app.logger.info("That file extension is not allowed")
-                    return redirect(request.url)
-
-    return render_template("public/upload_file.html")
+	if request.method=='POST':
+		if request.files:
+			if "filesize" in request.cookies:
+				if not allowed_filesize(request.cookies["filesize"]):
+					app.logger.info('not saved')
+					return jsonify({'message':'Too large size,400'})
+			else:
+				file=request.files["file"]
+				if allowed_file(file.filename):
+					full_filename = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
+					file.save(full_filename)
+					app.logger.info('saved')
+					return jsonify({'message':'Json received,200'})
+				else:
+					app.logger.info('not saved')
+					return jsonify({'message':'File extension not allowed,400'})
+	return render_template("public/upload_file.html")
