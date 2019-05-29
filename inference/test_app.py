439c26a4f91799eaa8b8ae533d31d18813a231f2
vinay <vinaypand5@gmail.com>
2019-05-29 19:43:46 +0530

    check removed

--- inference/test_app.py
+++
@@ -1,9 +0,0 @@
-import flask
-from app import app
-
-
-def test_upload():
-    client = app.test_client()
-    rsp = client.get('/upload_file')
-    html = rsp.get_data(as_text=True)
-    assert '<h1>Upload json</h1>' in html
