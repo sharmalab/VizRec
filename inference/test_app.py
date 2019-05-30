fbc54dc0ea0b478e65352c7024088495bcd3345b
vinay <vinaypand5@gmail.com>
2019-05-30 19:11:28 +0530

    fixed

--- inference/test_app.py
+++ inference/test_app.py
@@ -4,5 +4,5 @@ from app import app
 
 def test_upload():
     client = app.test_client()
-    response = client.post("/upload_file",data=json.dumps(dict(f='f')))
+    response = client.post("/upload_file", data=json.dumps(dict(f='f')))
     assert response.status_code == 200
