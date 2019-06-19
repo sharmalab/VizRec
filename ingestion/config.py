af143b3a9ab858ffbeeaa2d98b4777dcf4fadeff
vinay <vinaypand5@gmail.com>
2019-06-19 20:47:18 +0530

    Fixed docker

--- ingestion/config.py
+++ ingestion/config.py
@@ -30,7 +30,8 @@ class TestingConfig(Config):
     ALLOWED_EXTENSIONS = ["JSON"]
     MAX_CONTENT_LENGTH = 16 * 1024 * 100024
     # URI = 'mongodb://127.0.0.1:5000'
-    CLIENT = pymongo.MongoClient('172.17.0.1', 27017)
+    # MONGO_CLIENT = pymongo.MongoClient('mongodb://127.0.0.1:5000',27017)
+    CLIENT = pymongo.MongoClient('mongodb://DB:27017/vizrec' or 'localhost',27017)
     DB = CLIENT['test']
     VIZREC = DB.vizrec
     HOST = '0.0.0.0'
