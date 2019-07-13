fb51c82e05138c8937f0806fb40fad9c26515af4
vinay <vinaypand5@gmail.com>
2019-07-13 17:52:18 +0530

    removing node_modules

--- query/node_modules/@cubejs-backend/schema-compiler/compiler/UserError.js
+++
@@ -1,3 +0,0 @@
-class UserError extends Error {}
-
-module.exports = UserError;
--- query/node_modules/@cubejs-backend/schema-compiler/test/PrepareCompiler.js
+++
@@ -1,7 +0,0 @@
-const PrepareCompiler = require('../compiler/PrepareCompiler');
-
-exports.prepareCompiler = (content, options) => PrepareCompiler.prepareCompiler({
-  dataSchemaFiles: () => Promise.resolve([
-    { fileName: "main.js", content }
-  ])
-}, { adapter: 'postgres', ...options });
